909c2085a448d949a5e747455bca829d7a362156
AgustinLuporini <153564780+AgustinLuporini@users.noreply.github.com>
2024-11-03 22:37:00 -0300

    Add files via upload

---
+++ TP_Grafos/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.HashSet;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+       Grafo grafo = new Grafo();
+
+        //Crear lugares
+        Vertice FCiencieas = new Vertice("Facultad de ciencias");
+        Vertice FIngenieria = new Vertice("Facultad de ingenierías");
+        Vertice FArtes = new Vertice("Facultad de arte");
+        Vertice biblioteca = new Vertice("Biblioteca");
+        Vertice comedor = new Vertice("Comedor");
+
+        //Agregar lugares (Vértices)
+        grafo.agregarVertice(FCiencieas);
+        grafo.agregarVertice(FIngenieria);
+        grafo.agregarVertice(FArtes);
+        grafo.agregarVertice(biblioteca);
+        grafo.agregarVertice(comedor);
+
+        //Agregar trayectos (Aristas)
+        grafo.agregarArista(FCiencieas, FIngenieria, 30);
+        grafo.agregarArista(FCiencieas, comedor, 15);
+        grafo.agregarArista(FIngenieria, comedor,20);
+        grafo.agregarArista(FIngenieria, biblioteca, 13);
+        grafo.agregarArista(FCiencieas, biblioteca, 22);
+        grafo.agregarArista(comedor, biblioteca, 28);
+        grafo.agregarArista(biblioteca, FArtes, 5);
+        grafo.agregarArista(FArtes, comedor, 10);
+
+        Kruskal kruskal = new Kruskal();
+        List<Arista> arbolExpansionMaxima = kruskal.encontrarMST(grafo);
+
+        System.out.println("Trayectos entre puntos seleccionados:");
+        for (Arista arista : arbolExpansionMaxima) {
+            System.out.println(arista.getOrigen() + " <-----> " + arista.getDestino() + " | Transito: " + arista.getPeso());
+        }
+
+
+    }
+
+
+}
